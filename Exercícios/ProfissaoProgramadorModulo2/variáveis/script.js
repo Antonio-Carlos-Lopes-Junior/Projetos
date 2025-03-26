e72a5af30f85de713818c402f47eb5df55232a4d
Antonio-Carlos-Lopes-Junior <antoniocljunior@gmail.com>
2025-03-26 19:51:03 -0300

    aula sobre variáveis

---
+++ Exercícios/ProfissaoProgramadorModulo2/variáveis/script.js
@@ -0,0 +1,17 @@
+// a variável só pode começar por letra, $ ou _.
+// os únicos cacacteres especiais permitidos nas variáveis são o $ e o _.
+// a variável pode ter números, mas não pode começar.
+// usar o padrão camel case, onde 1ª letra da variável é minúscula e a 1ª letra das demais palavras da
+// variável é maiúscula.
+// o javascript é case sensitive.
+
+// tipo let pode redeclarar e const não pode.
+
+let fraseAlert = 'frase do alert';
+// alert(fraseAlert);
+
+let fraseConsole = 'frase do console';
+console.log(fraseConsole);
+
+const fraseConst = 'frase const';
+console.log(fraseConst);
