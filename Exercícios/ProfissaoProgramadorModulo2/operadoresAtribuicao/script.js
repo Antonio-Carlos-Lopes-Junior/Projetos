353ba1dc7996d785c9ab8d985b547e125a334ba0
Antonio-Carlos-Lopes-Junior <antoniocljunior@gmail.com>
2025-03-29 19:58:06 -0300

    aula sobre operadores atribuição

---
+++ Exercícios/ProfissaoProgramadorModulo2/operadoresAtribuicao/script.js
@@ -0,0 +1,20 @@
+let numero1 = 6;
+const numero2 = 2;
+
+//operadores de atribuição
+
+// =
+// +=
+// x += y --> x = x + y
+// -=
+// x -= y --> x = x - y
+// *=
+// x *= y --> x = x * y
+
+const adicao = numero1 += numero2; // numero1 = numero1 + numero2
+const subtracao = numero1 -= numero2; // numero1 = numero1 - numero2
+const multiplicacao = numero1 *= numero2; // numero1 = numero1 * numero2
+
+console.log(adicao);
+console.log(subtracao);
+console.log(multiplicacao);
