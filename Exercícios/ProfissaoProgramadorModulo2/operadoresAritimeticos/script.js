88d8cee23abc122171323c1c0ec75f675a775eb3
Antonio-Carlos-Lopes-Junior <antoniocljunior@gmail.com>
2025-03-29 19:26:13 -0300

    aula sobre operadores aritimeticos

---
+++ Exercícios/ProfissaoProgramadorModulo2/operadoresAritimeticos/script.js
@@ -0,0 +1,28 @@
+let numero1 = 6;
+const numero2 = 2;
+
+//operadores arimiméticos
+const adicao = numero1 + numero2; //8
+const subtracao = numero1 - numero2; //4
+const multiplicacao = numero1 * numero2; //12
+const divisao = numero1 / numero2; //3
+
+const preincremento = ++numero1; //7
+const posdecremento = numero1--; //7
+const predecremento = --numero1; //5
+const posincremento = numero1++; //5
+
+const modulo = numero1 % numero2; //0
+
+const exponencial = numero1 ** numero2; // 6 / 6 = 36
+
+console.log(adicao);
+console.log(subtracao);
+console.log(multiplicacao);
+console.log(divisao);
+console.log(preincremento);
+console.log(posdecremento);
+console.log(predecremento);
+console.log(posincremento);
+console.log(modulo);
+console.log(exponencial);
