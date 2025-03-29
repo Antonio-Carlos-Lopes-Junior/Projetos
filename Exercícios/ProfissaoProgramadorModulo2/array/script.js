1032ae3ebff21c577b543d8f92086c2609207d8a
Antonio-Carlos-Lopes-Junior <antoniocljunior@gmail.com>
2025-03-29 17:11:11 -0300

    aula sobre array

---
+++ Exercícios/ProfissaoProgramadorModulo2/array/script.js
@@ -0,0 +1,24 @@
+const vetor = ['Paulo', 'Marta', 'Andressa'];
+
+console.log(vetor);
+console.log(vetor[0]);
+console.log(vetor[1]);
+console.log(vetor[2]);  
+
+const matriz = [
+    ['honda', 'toyota', 'hyundai'],
+    ['Paulo', 'Andressa'],
+    ['engenheiro', 'programador']
+]
+
+console.log(matriz);
+console.log(matriz[0]);
+console.log(matriz[0][0]);
+console.log(matriz[0][1]);
+console.log(matriz[0][2]);
+console.log(matriz[1]);
+console.log(matriz[1][0]);
+console.log(matriz[1][1]);
+console.log(matriz[2]);
+console.log(matriz[2][0]);
+console.log(matriz[2][1]);
