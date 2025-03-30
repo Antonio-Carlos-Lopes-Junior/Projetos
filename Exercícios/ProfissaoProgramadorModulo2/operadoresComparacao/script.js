e51aa2c5ff8feb565c2835988eacc644cf32efcd
Antonio-Carlos-Lopes-Junior <antoniocljunior@gmail.com>
2025-03-30 16:10:26 -0300

    aula sobre operadores comparação

---
+++ Exercícios/ProfissaoProgramadorModulo2/operadoresComparacao/script.js
@@ -0,0 +1,16 @@
+const numero1 = 6;
+const numero2 = 2;
+const numero3 = '6';
+
+// operadores de comparação
+
+console.log(numero1 > numero2); // true
+console.log(numero1 < numero2); // false
+console.log(numero1 >= numero2); // true
+console.log(numero1 <= numero2); // false
+console.log(numero1 == numero2); // false
+console.log(numero1 != numero2); // true
+console.log(numero1 === numero3); // false - verifica se são identicos
+console.log(numero1 !== numero3); // true - verifica se são identicos
+console.log(numero1 == numero3); //true - só verifica se são iguais mesmo de tipos diferentes
+console.log(numero1 != numero3); //false - só verifica se são iguais mesmo de tipos diferentes
