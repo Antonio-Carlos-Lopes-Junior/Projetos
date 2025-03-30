184f765d9fa5a12bbcce283767e4c6ca0381d22f
Antonio-Carlos-Lopes-Junior <antoniocljunior@gmail.com>
2025-03-30 16:28:13 -0300

    aula sobre operador ternário

---
+++ Exercícios/ProfissaoProgramadorModulo2/operadorTernario/script.js
@@ -0,0 +1,9 @@
+console.log(true ? 'Retornou true' : 'Retornou false');
+console.log(2 ? 'Retornou true' : 'Retornou false');
+console.log(' ' ? 'Retornou true' : 'Retornou false');
+console.log(false ? 'Retornou true' : 'Retornou false');
+console.log(0 ? 'Retornou true' : 'Retornou false');
+console.log(NaN ? 'Retornou true' : 'Retornou false');
+console.log(false ? 'Retornou true' : 'Retornou false');
+console.log(undefined ? 'Retornou true' : 'Retornou false');
+console.log('' ? 'Retornou true' : 'Retornou false');
